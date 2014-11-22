c1933c2ec7022b3133c158387040cc1c2c4df9ca
Utsav <ukanani@salesforce.com>
2014-11-22 01:18:57 -0800

    Setting up intial build files

---
+++ src/main/java/co/subsnap/App.java
@@ -0,0 +1,13 @@
+package co.subsnap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
