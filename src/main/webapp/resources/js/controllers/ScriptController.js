bccd0d54fcd97842beeddaa72a4b4597a2d8a3f7
Dale Knauss <daleknauss@gmail.com>
2014-11-23 14:21:47 -0800

    Integrate

--- src/main/webapp/resources/js/controllers/ScriptController.js
+++ src/main/webapp/resources/js/controllers/ScriptController.js
@@ -7,21 +7,39 @@
 
 var subSnapControllers = angular.module('subSnapControllers', ['ngTable','ui.bootstrap']);
 
-subSnapControllers.controller('ScriptController', ['$scope', '$http', '$modal', 'SendEmail',
-  function ($scope, $http, $modal, SendEmail) {
+subSnapControllers.controller('ScriptController', ['$scope', '$http', '$modal', 'SendGridEmail', 'Project', 'SendEmail',
+  function ($scope, $http, $modal, SendGridEmail, Project, SendEmail) {
     $scope.goNext = function (hash) { 
         $location.path(hash);
     };
     
-    $scope.projects = [
-        {projectId: 'safasdgvsd', name: "The Social Network", writer: "Aaron Sorkin", director: "David Fincher", star: "Jesse Eisenberg", dateSubmitted: "1416703568837"},
-        {projectId: 'safsafas', name: "Requim For A Dream", writer: "Hubert Selby Jr.", director: "Darren Aronofsky", star: "Jarad Leto",  dateSubmitted: "1416703568837"},
-        {projectId: 'safasvdgvsd', name: "Eternal Sunshine of the Spotless Mind", writer: "Charlie Kaufman", director: "Michel Gondry", star: "Jim Carrey", dateSubmitted: "1416703568837"},
-        {projectId: 'safaasfassdgvsd', name: "Reservoir Dogs", writer: "Quentin Tarantino", director: "Quentin Tarantino", star: "Harvey Keitel", dateSubmitted: "1416703568837"},
-        {projectId: 'safaavasdgvsd', name: "Snatch", writer: "Guy Ritchie", director: "Guy Ritchie", star: "Brad Pitt", dateSubmitted: "1416703568837"}
-    ];
+    // $scope.projects = [
+    //     {projectId: 'safasdgvsd', projectName: "The Social Network", writer: "Aaron Sorkin", director: "David Fincher", star: "Jesse Eisenberg", dateSubmitted: "1416703568837"},
+    //     {projectId: 'safsafas', projectName: "Requim For A Dream", writer: "Hubert Selby Jr.", director: "Darren Aronofsky", star: "Jarad Leto",  dateSubmitted: "1416703568837"},
+    //     {projectId: 'safasvdgvsd', projectName: "Eternal Sunshine of the Spotless Mind", writer: "Charlie Kaufman", director: "Michel Gondry", star: "Jim Carrey", dateSubmitted: "1416703568837"},
+    //     {projectId: 'safaasfassdgvsd', projectName: "Reservoir Dogs", writer: "Quentin Tarantino", director: "Quentin Tarantino", star: "Harvey Keitel", dateSubmitted: "1416703568837"},
+    //     {projectId: 'safaavasdgvsd', projectName: "Snatch", writer: "Guy Ritchie", director: "Guy Ritchie", star: "Brad Pitt", dateSubmitted: "1416703568837"}
+    // ];
+    
+    $scope.projects = Project.query({}, function (projects) {
+        for (var i = projects.length - 1; i >= 0; i--) {
+            projects[i].showLog = false;
+        }
+        return projects;
+    });
 
-
+    // $scope.getSends = function (project) {
+    //     sendEmail.$GetSends({ id: project.projectId }, function (send) {
+    //         debugger;
+    //     });
+    // };
+    $scope.toggleLogs = function (projectIndex) {
+        project = $scope.projects[projectIndex];
+        $scope.projects[projectIndex].showLog = !project.showLog;
+    };
+    $scope.hasLogs = function (project) {
+        return project.sends.length;
+    };
     $scope.sendEmail = function(project) {
         var resultPromise = $modal.open({
             templateUrl: 'scripts/send-modal.html',
@@ -36,29 +54,32 @@ subSnapControllers.controller('ScriptController', ['$scope', '$http', '$modal', 
         });
 
         resultPromise.result.then(function(message) {
-            var send = new SendEmail({
-                to: message.email,
-                toName: message.name,
-                subject: message.title,
-                text: message.body,
-                                    api_user: 'subsnap',
-                    api_key: '5195Wint!',
-                    from: 'Jenny@subsnap.com'
-            }, function () {
-                alert(response);
+            var sendEmail = new SendEmail();
+            sendEmail.$PostSend({
+                projectId: message.projectId,
+                sendMail: [
+                    {
+                        sendEmailName: message.toName,
+                        sendEmailAddress: message.email,
+                        isWatermarked: message.isWatermarked,
+                        sendEmailTitle: message.title,
+                        sendEmailBody: message.body
+                    }
+                ]
             });
-            send.$save();
 
-
-            // Movie.get({ id: $scope.id }, function() {
-            //   // $scope.entry is fetched from server and is an instance of Entry
-            //     $scope.entry.data = 'something else';
-            //     $scope.entry.$delete(function() {
-            //     //gone forever!
+            // var send = new SendGridEmail({
+            //     to: message.email,
+            //     toName: message.name,
+            //     subject: message.title,
+            //     text: message.body,
+            //     api_user: 'subsnapAPI',
+            //     api_key: '5195Wint!',
+            //     from: 'Jenny@subsnap.com'
+            // }, function () {
+            //     alert(response);
             // });
-// });
-            // alert('email sent!!!' + message);
-            // @TODO post result to server
+            // send.$save();
         });
     };
 
@@ -67,7 +88,7 @@ subSnapControllers.controller('ScriptController', ['$scope', '$http', '$modal', 
 subSnapControllers.controller('EmailController', function ($scope, $modalInstance, project) {
 
     var assembleMessage = function (project) {
-        var string = 'As discussed, attached please find ' + project.name.toUpperCase();
+        var string = 'As discussed, attached please find ' + project.projectName.toUpperCase();
         if (project.writer) {
             string += ' written by ' + project.writer;
         }
@@ -86,10 +107,10 @@ subSnapControllers.controller('EmailController', function ($scope, $modalInstanc
     $scope.project = project;
     $scope.message = {
         projectId: project.projectId,
-        name: '',
+        toName: '',
         email: '',
         isWatermarked: false,
-        title: '',
+        title: project.projectName.toUpperCase(),
         body: assembleMessage(project)
     };
 
@@ -97,7 +118,7 @@ subSnapControllers.controller('EmailController', function ($scope, $modalInstanc
 
 
     $scope.firstName = function () {
-        var name = $scope.message.name;
+        var name = $scope.message.toName;
         if (name) {
             return name.split(' ')[0] + ',';
         }
--- src/main/webapp/resources/js/services.js
+++ src/main/webapp/resources/js/services.js
@@ -4,33 +4,49 @@
 
 var AppServices = angular.module('subSnapServices', ['ngResource']);
 
-// AppServices.factory('Project', ['$resource',
-//     function($resource){
-//         return $resource('projects', {}, {
-//             query: { 
-//                 method:'GET', 
-//                 params:{projectId:'projects'}, 
-//                 isArray: true}
-//             }
-//         );
-//     }
-// ]);
+AppServices.factory('Project', ['$resource',
+    function($resource){
+        return $resource('/projects/', {}, {
+            query: { 
+                method:'GET', 
+                // params: {projectId:'projects'}, 
+                isArray: true
+            }
+        });
+    }
+]);
 
-// AppServices.factory('Message', ['$resource',
-//     function($resource){
-//         return $resource('messages', {}, {
-//             query: { 
-//                 method:'POST', 
-//                 params: { 
-//                     projectId:'projects' 
-//                 }, 
-//                 isArray: true
-//             }
-//         });
-//     }
-// ]);
+AppServices.factory('SendEmail', ['$resource',
+    function($resource) {
+        return $resource('/sends/:id', {}, {
+            PostSend: { method:'POST', params: {} },
+            GetSends: { method: 'GET', params: { id: 0 } }
 
-AppServices.factory('SendEmail', ['$resource', 
+
+            // query: { 
+            //     params: { 
+            //         projectId:'projects' 
+            //     }, 
+            //     isArray: true
+            // }
+        });
+    }
+]);
+
+// function TodoService($resource) {
+//     var src = $resource('api/1/todo/:id:cmd',
+//               {id: "@id", cmd: "@cmd"}, //parameters default
+//               {
+//                 ListTodos: { method: "GET", params: {} },
+//                 GetTodo: { method: "GET", params: { id: 0 } },                            
+//                 CreateTodo: { method: "POST", params: { content: "", order: 0, done: false } },
+//                 UpdateTodo: { method: "PATCH", params: { /*...*/ } },
+//                 DeleteTodo: { method: "DELETE", params: { id: 0 } },
+//                 ResetTodos: { method: "GET", params: { cmd: "reset" } },
+//               });
+
+
+AppServices.factory('SendGridEmail', ['$resource', 
     function ($resource) {
         return $resource('https://api.sendgrid.com/api/mail.send.json', {}, {
             query: { 
